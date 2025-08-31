6120d8b50613e69cd0e8ba56e387a95267515d40
but212 <wjddlf211@naver.com>
2025-08-31 15:33:11 +0900

    feat: create SearchCardList component with SearchForm and SearchedList subcomponents

---
+++ src/components/SearchCardList/SearchCardList.tsx
@@ -0,0 +1,12 @@
+import "./SearchCardList.css";
+import SearchForm from './SearchForm/SearchForm';
+import SearchedList from './SearchedList/SearchedList';
+
+const SearchCardList = () => {
+  return (
+    <>
+      <SearchForm />
+      <SearchedList />
+    </>
+  );
+};
