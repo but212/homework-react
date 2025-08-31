30f7a6715e9e6486968f5f730b8da98d365fde61
but212 <wjddlf211@naver.com>
2025-08-31 12:01:05 +0900

    chore(App): format App.tsx with formatter and add App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import './styles/App.css';
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold">템플릿 페이지</h1>
-      <p className="text-xl">변경사항을 적용해보세요.</p>
+      <h1 className='text-3xl font-bold'>템플릿 페이지</h1>
+      <p className='text-xl'>변경사항을 적용해보세요.</p>
     </>
   );
 };
